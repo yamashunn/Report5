1713e950fabc688d5a19a734cad74b56813db963
yamashunn <e205733@ie.u-ryukyu.ac.jp>
2020-12-19 18:04:15 +0900

    step2

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,11 @@
-public class Test{
+public class test {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("以下のようなエラーが出ました");
+            System.out.println(e.getMessage());
+        }
 
     }
 }
